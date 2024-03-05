ac27a383d2058c700c0acfa5fc946e575eb22294
martynasj <martyjankauskas@gmail.com>
2024-03-05 16:31:07 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your-package-name',
+    version='0.1',
+    packages=["kerykeion"],
+    # Or if the packages are top-level directories, you can specify them explicitly
+    # packages=['exemples', 'kerykeion']
+)
