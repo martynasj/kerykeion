4c7b1cd06d7c9c42e7787201aa5da13d7dcdab8e
martynasj <martyjankauskas@gmail.com>
2024-03-05 22:52:23 +0200

    rm setup files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='your-package-name',
-    version='0.2',
-    packages=find_packages(),
-    # Or if the packages are top-level directories, you can specify them explicitly
-    # packages=['exemples', 'kerykeion']
-)
